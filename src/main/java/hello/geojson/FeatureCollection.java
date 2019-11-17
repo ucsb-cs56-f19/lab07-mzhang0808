d2dd641ee54867049f3996daa87f9adaa78623f6
Michael Zhang <mzhang0808@gmail.com>
2019-11-16 16:08:33 -0800

    mz - final cleanup 3.0

--- src/main/java/hello/geojson/FeatureCollection.java
+++ src/main/java/hello/geojson/FeatureCollection.java
@@ -3,34 +3,34 @@ package hello.geojson;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
-
 public class FeatureCollection {
-	public String type;
-	private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
-	public List<Feature> features;
-	public Metadata metadata;
+    private static Logger logger = LoggerFactory.getLogger(FeatureCollection.class);
 
-	public static FeatureCollection fromJSON(String json) {
-		
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    public String type;
+    public Metadata metadata;
+    public List<Feature> features;
 
-			FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
-			return featureCollection;
-		} catch (JsonProcessingException jpe) {
-			logger.error("JsonProcessingException:" + jpe);
-			return null;
-		} catch (Exception e) {
-			logger.error("Exception:" + e);
-			return null;
-		}
-	}
+    public static FeatureCollection fromJSON(String json) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
+            FeatureCollection featureCollection = objectMapper.readValue(json, FeatureCollection.class);
+            return featureCollection;
+        } catch (JsonProcessingException jpe) {
+            logger.error("JsonProcessingException:" + jpe);
+            return null;
+        } catch (Exception e) {
+            logger.error("Exception:" + e);
+            return null;
+        }
+
+    }
 }
